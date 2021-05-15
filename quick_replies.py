b97a3c50c9bd01821eff8b71583ea0aa45aba816
pd-newton <pd-newton@ponddy-edu.net>
2021-05-15 22:06:12 +0800

    feat: function of show quick replies

---
+++ quick_replies.py
@@ -0,0 +1,36 @@
+import requests
+import orm
+
+
+def quick_replies(sender_id, headers, params, state, **kwargs):
+    print("quick_replies---------------------------")
+    db = orm.SessionLocal()
+    user = db.query(orm.User).filter(orm.User.fb_id == sender_id).one_or_none()
+    print(user.state_id, 'inner------')
+    sub_states = db.query(orm.State).filter(
+        orm.State.parent_id == user.state_id
+    ).all()
+    if sub_states:
+        data = {
+            "recipient": {
+                "id": user.fb_id
+            },
+            "messaging_type": "RESPONSE",
+            "message": {
+                "text": state.label,
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": sub_state.label,
+                        "payload": sub_state.name,
+                    }
+                    for sub_state in sub_states
+                ]
+            }
+        }
+        requests.post(
+            'https://graph.facebook.com/v10.0/me/messages',
+            headers=headers,
+            params=params,
+            json=data
+        )
